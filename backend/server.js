9a3c1a264e1bd77603639079bf7a14bb5ee61de0
Eugene <eugene.pa77@gmail.com>
2024-06-28 16:55:59 +0900

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,16 @@
 const controller = require('./controller/controller');
 
-let result;
-const getData = (data) => {
-    result = data;
-}
-
-controller.getTickerPriceForSocket(getData);
-setInterval(() => {
-    controller.getTickerPriceForSocket(getData);
-}, 15000);
-
-
 exports = module.exports = server = (io) => {
     io.on('connection', (socket) => {
         console.log(`--- A socket ${socket.id} connected! ---`);
-        socket.emit('send_token', result);
+        
+        const getData = (data) => {
+            socket.emit('send_token', data);
+        }
+        controller.getTickerPriceForSocket(getData);
         setInterval(() => {
-            socket.emit('send_token', result);
-        }, 5000);
-
+            controller.getTickerPriceForSocket(getData);
+        }, 30000);
         socket.on('send_data', (data) => {
             socket.emit('receive_data', data);
         });
